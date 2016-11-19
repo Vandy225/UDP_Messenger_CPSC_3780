898a0632df1ac1df4c532ce576c6b5200e7aaefc
CharlieWoldBravo <lance96chisholm@gmail.com>
2016-11-18 18:53:18 -0700

    This should work now.. i hope...

--- client.py
+++ client.py
@@ -1,15 +1,16 @@
 import socket
 import pickle
 import threading
-import queue
 import time
 import sys
 
 
 '''Servers IP address'''
-UDP_IP = "142.66.140.23"
-UDP_PORT = 5005
-UDP_ACK_PORT = 5006
+SERVER_IP = "142.66.140.23"
+UDP_PORT = 5007
+UDP_ACK_PORT = 5009
+SERVER_PORT = 5005
+SERVER_ACK_PORT = 5006
 seq_num = 0
 message = { 'seq' : seq_num, 'type' : '' , 'source' : socket.gethostbyname(socket.gethostname()), 'destination' : '142.66.140.24', 'payload' : ''}
 try:
@@ -19,25 +20,43 @@ except socket.error:
     print 'Failed to create sockets.'
     sys.exit()
 
-sock.bind((UDP_IP,UDP_PORT))
-sock2.bind((UDP_IP,UDP_ACK_PORT))
+#Setting up the LISTENING udp portion
+sock.bind((SERVER_IP,UDP_PORT))
+#sock2.bind((SERVER_IP,UDP_ACK_PORT))
 
-def user_listen():
-    msg = { 'seq' : seq_num, 'type' : 'get' , 'source' : socket.gethostbyname(socket.gethostname()), 'destination' : UDP_IP, 'payload' : ''}
-    sock.sendto(pickle.dumps(msg).encode('utf-8'),(UDP_IP,UDP_PORT))
-    while data, address = sock.recvfrom(1024):
+def user_listen(data, address):
+    global SERVER_IP
+    global SERVER_PORT
+    msg = { 'seq' : seq_num, 'type' : 'get' , 'source' : socket.gethostbyname(socket.gethostname()), 'payload' : ''}
+    sock.sendto(pickle.dumps(msg).encode('utf-8'),(SERVER_IP,SERVER_PORT))
+    while data:
         message = pickle.loads(data.decode('utf-8'))
-        print "Message: ", message['payload'] #this may change... JOSH
+        print "Received Message: ", message['payload'] #this may change... JOSH
+        if(message['payload'] == 'No messages'):
+            break
+        
 
 def send_mode():
-    message['payload'] = raw_input("Message: ")
-    message['type'] = 'send'
-    sock.sendto(pickle.dumps(message).encode('utf-8'),(UDP_IP,UDP_PORT))
-    seq_num = seq_num + 1
+    while True:
+        thing = raw_input("Message: ")
+        if(thing == ''):
+            pass
+        else:
+            global SERVER_IP 
+            global SERVER_PORT
+            message['payload'] = thing
+            message['type'] = 'send'
+            sock.sendto(pickle.dumps(message).encode('utf-8'),(SERVER_IP,SERVER_PORT))
+            global seq_num 
+            seq_num += 1
+        
     
+
+t = threading.Thread(target = send_mode(), args=[])
+t.start()
+
 while True:
-    
-    t = threading.Thread(target = send_mode(), args=[])
-    t2 = threading.Thread(target = user_listen(), args=[])
-    t.start()
+    data, address = sock.recvfrom(1024)
+    t2 = threading.Thread(target =user_listen(), args=[data, address])
     t2.start()
+    
--- server.py
+++ server.py
@@ -1,15 +1,13 @@
 import socket
 import pickle
 import threading
-import queue
 import time
 import sys
 
 UDP_PORT = 5005
 UDP_ACK_PORT = 5006
-SERVER_ADDRESS = socket.gethostbyname(socket.gethostname())
+SERVER_ADDRESS = str(socket.gethostbyname(socket.gethostname()))
 SERVER_ID = 'Server Host'
- msg = { 'seq' : , 'type' : '' , 'source' : socket.gethostbyname(socket.gethostname()), 'destination' : , 'payload' : ''}
 
 try:
     sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
@@ -24,46 +22,57 @@ sock2.bind(('', UDP_ACK_PORT))
 print "server started on port: ", UDP_PORT
 
 #data structure for messages per client. { client ip : [list of messages] }
-message_list = { client_ip : [] }
+message_list = { 'client_ip' : [] }
 client_list = {}
 
-message_queue = queue.Queue(maxsize = 0)
-ack_queue = queue.Queue(maxsize = 0)
 
 #iterate over the list of stored messages for a particular client.
 #Send each message individually.
-def deliver_messages(dest, msg):
-    if not message_list[dest]:
-        mess = { 'seq' : , 'type' : '' , 'source' : socket.gethostbyname(socket.gethostname()), 'destination' : dest, 'payload' : 'No messages.'}
-        sock.sendto(pickle.dump(mess).encode('utf-8'),(dest,UDP_PORT))
+def deliver_messages(dest, msg, address):
+    if dest not in message_list:
+        print 'No messages for you...'
+        hot_mess = { 'seq' : '', 'type' : '' , 'source' : SERVER_ADDRESS, 'destination' : dest, 'payload' : 'No messages.' }
+        sock.sendto(pickle.dumps(hot_mess).encode('utf-8'), (address[0], address[1]))
     else:
         for client_ip in message_list:
+            print 'nerds 4 a bttr worl...'
             if(dest == client_ip):
+                print 'nested usa...'
                 for message in client_ip:
-                    sock.sendto(pickle.dump(message).encode('utf-8'),(dest,UDP_PORT))
+                    print 'nestesd hell...'
+                    sock.sendto(pickle.dumps(message).encode('utf-8'),(address[0],address[1]))
 
 #store the message for destination in the list. 
 def store_message(dest, msg):
-    message_list[dest].append(msg)
+    if dest in message_list:
+        message_list[dest] = msg
 
 def receive_message(data, address):
+    print 'Attempting to decode the death star plans...'
     message = pickle.loads(data.decode('utf-8'))#This may change
+    print 'MESSAGE RAW: ', str(message)
 
     if(message['type'] == 'send'):
+        print 'Message recieved, storing...'
         store_message(message['destination'], message)
     if(message['type'] == 'get'):
-        deliver_messages(message['destination'],  message)
+        print 'Deliver messages...'
+        deliver_messages(message['source'],  message, address)
     
 def listen_mode():
-    while True:
-        data, address = sock.recvfrom(1024)
-        data2, addr = sock2.recvfrom(1024)
+    print 'listen for data....'
+    data, address = sock.recvfrom(1024)
+    #data2, addr = sock2.recvfrom(1024)
+    print 'thread start...'
+    t = threading.Thread(target = receive_message, args=[data, address])
+    #t2 = threading.Thread(target = '''Ack_reception function''', args = [data2, addr])
+    
+    t.start()
+    #t2.start()
+    
 
-        t = threading.Thread(target = receive_message, args=[data, address])
-        t2 = threading.Thread(target = '''Ack_reception function''', args = [data2, addr])
-
-        t.start()
-        t2.start()
+while True:
+    listen_mode()
 
 '''
 def check_inbox( inbox ):
