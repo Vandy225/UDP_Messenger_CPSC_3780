7305d387c196b2a8d0e73b6a660db2eea63dbb2d
CharlieWoldBravo <lance96chisholm@gmail.com>
2016-11-28 16:32:32 -0700

    Appetizer of spaghetti to come....

--- client.py
+++ client.py
@@ -1,97 +1,57 @@
-import pickle
 import socket
-import sys
-import multiprocessing as mp
-from multiprocessing import Process
+import pickle
 import sys
 import os
+import time
 
-'''Servers IP address'''
-SERVER_IP = "10.76.134.106"
-UDP_PORT = 5007
-UDP_ACK_PORT = 5009
+
+SERVER_IP = '142.66.140.34'
+UDP_PORT = 5006
 SERVER_PORT = 5005
-SERVER_ACK_PORT = 5006
 seq_num = 0
-message = { 'seq' : seq_num, 'type' : '' , 'source' : socket.gethostbyname(socket.gethostname()), 'destination' : '10.76.69.237', 'payload' : ''}
+user_name = ''
+routing_table = {} #this is for the purpose of translating user_names into ip's
 
-def user_listen(sock):
-    global SERVER_IP
-    global SERVER_PORT
-    msg = { 'seq' : '', 'type' : 'get' , 'source' : socket.gethostbyname(socket.gethostname()), 'payload' : ''}
-    print ("Trying to send get...")
-    sock.sendto(pickle.dumps(msg),(SERVER_IP,SERVER_PORT))
-    print ("Waiting for server response...")
-    data, address = sock.recvfrom(1024)
-    message_list = pickle.loads(data) #now the client is getting the entire message list, need to iterate through
-    ack_list = []
-    if message_list:
-        for index in message_list:
-            print ("Received Message: " + str(index['payload']))#this may change... JOSH
-            ack_list.append(index['seq'])
-        ack_handle(ack_list,sock)
-    else:
-        print("No Messages right now. Check back later.")
-    #we need to be able to get the message dump, right now we are only
-    #getting one messgage at a time
-    #may use a loop to iterate through the messages --> need to implements ACKS
-    #or use the seq number to stop iterating through the for loop
-
-def send_mode(sock):
-    #Open the same stdin as main.
-    #sys.stdin = os.fdopen(fileno)
-    while True:
-        #something is wrong with one of the previous print statements.
-        thing = input("Message: ")
-        if(thing == ''):
-            user_listen(sock)
-        else:
-            global SERVER_IP 
-            global SERVER_PORT
-            global seq_num
-            message['seq'] = seq_num
-            print ("sending message with seq_num =" + str(message['seq']))
-            message['payload'] = thing
-            message['type'] = 'send'
-            message['seq'] = seq_num
-            sock.sendto(pickle.dumps(message),(SERVER_IP,SERVER_PORT))
-            seq_num += 1
+def handshake(sock):
+    global user_name
+    global seq_num
+    user_name = raw_input("User Name: ")
+    message = {'seq' : seq_num, 'type': 'handshake', 'source': socket.gethostbyname(socket.gethostname()), 'user_name': user_name}
+    print "Handshaking with server..."
+    sock.sendto(pickle.dumps(message), (SERVER_IP, SERVER_PORT))
+    #increment sequence number, may use later
+    seq_num += 1
+    print "waiting on server response..."
+    time.sleep(0.5)
+    #now we need to check if our entry for username was good
+    user_good = False
+    #keep making the suer enter a user name until good
+    while user_good == False:
+        data, address = sock.recvfrom(10240)
+        user_ack = pickle.loads(data)
+        if user_ack['type'] == 'user_good':
+            print "name registration successful"
+            user_good = True
+        elif (user_ack['type'] == 'user_error'):
+            print "username is taken, try again"
+            handshake(sock)
+            
 
 
-def ack_handle(listy,sock):
-    global SERVER_ACK_PORT
-    global SERVER_IP
-    ack = {'seq': '', 'type': 'ack', 'source': socket.gethostbyname(socket.gethostname()), 'destination': '','payload': listy}
-    sock.sendto(pickle.dumps(ack),(SERVER_IP,SERVER_PORT))
 
-def main():
-    #fn = sys.stdin.fileno()
+#main function
+if __name__ == '__main__':
     try:
-        sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        #bind the socket for listening for the server
+        sock.bind(('', UDP_PORT))
     except socket.error:
-        print ("Failed to create sockets.")
+        #if socket creation failed, notify and break
+        print "Failed to create socket"
         sys.exit()
+    #try and handshake with the server
+    handshake(sock)
 
-    #Setting up the LISTENING udp portion
-    sock.bind((socket.gethostbyname(socket.gethostname()),UDP_PORT))
-
-    user_listen(sock)
-    while True:
-        send_mode(sock)
-    #while True:
-    '''print ("starting process 1")
-    p1 = Process(target = user_listen, args=(sock,fn,))
-    p1.start()
-    print("Process 1 has started.")
-
-    print ("starting process 2")
-    p2 = Process(target = send_mode, args=(sock,fn,))
-    p2.start()
-    print ("process 2 has started")'''
-
-
-
-#run main program
-if(__name__ == "__main__"):
-    mp.set_start_method('spawn')
-    main()
+        
+    
+           
--- server.py
+++ server.py
@@ -1,156 +1,147 @@
 import socket
 import pickle
-import threading
-from multiprocessing import Queue, Process
 import time
 import sys
 
-UDP_PORT = 5005
-UDP_ACK_PORT = 5006
+CLIENT_PORT = 5006
+SERVER_PORT = 5005
 SERVER_ADDRESS = str(socket.gethostbyname(socket.gethostname()))
-SERVER_ID = 'Server Host'
-message_queue = Queue() # declare the message queue
-message_list = { 'client_ip' : [] } #THIS IS ACTUALLY A MESSAGE DICTIONARY
-message_queue.put(message_list) # now we have an empty dictionary in the queue
+message_inbox = {} # this will be the replacement for the queue
+client_directory = {} #this will be a dict of user_names : ip addresses that this server hosts
+neighbor1 = '' #*
+# neighbor2 = ''
 
-def receive_message(message_queue):
-    data, address = sock.recvfrom(1024)  # listen on socket for messages
-    print("Attempting to decode the death star plans...")
-    message = pickle.loads(data)  # load received data into message
-    print("MESSAGE RAW: ", str(message))  # show the contents of the message for now
-    # if the message is of type 'send', output flag, call store_message
-    if (message['type'] == 'send'):
-        print("Message recieved, storing...")
-        '''ps = Process(target = store_message, args=(message['destination'], message, message_queue,))
-        ps.start()
-        ps.join()
-        ps.terminate()'''
-        store_message(message['destination'], message, message_queue)
-    if (message['type'] == 'get'):
-        print("Deliver messages...")
-        '''pdm = Process(target =deliver_messages, args=(message['source'], message, address, message_queue,))
-        pdm.start()
-        pdm.join()
-        pdm.terminate()'''
-        deliver_messages(message['source'], message, address, message_queue)
-    if (message['type'] == 'ack'):
-        '''pha = Process(target=handle_acknowledgement,args=(message['payload'], message['source'], message_queue,))
-        pha.start()
-        pha.join()
-        pha.terminate()'''
-        handle_acknowledgement(message['payload'], message['source'], message_queue)
+# the format for pairs in the table is:
+# routing_table = {user_name: [server_host_ip, hops, user_name's ip address]}
+routing_table = {} #dictionary that tells a client which server to send messages to
 
-    # iterate over the list of stored messages for a particular client.
-    # Send each message individually.
-def deliver_messages(dest, msg, address, message_queue):
-    message_dict = message_queue.get()
-    if dest not in message_dict:
-        print("Not in the dictionary yet...")
-        # send a no message response. Inbox is empty
-        hot_mess = []
-        sock.sendto(pickle.dumps(hot_mess), (dest, address[1]))
-    else:
-        # We are going to either want to fix client to listen properly, or make the server send all client dicts in a array or something.
-        client_msgs = message_dict[dest]
-        print("client msgs to deliver" + str(client_msgs))
-        sock.sendto(pickle.dumps(client_msgs), (dest, address[1]))
-    message_queue.put(message_dict)
-        # wait for some amount of time in order to get all the acks from client for messages in sent list.
-        # Waiting for ACKS needs to happen right here. An ACK function should be used. The message queue will not
-        # be used by the ACK handler
-        # Will be waiting to clear the message list for the client.
+lifetime_max = 1 #for now...
 
-    # store the message for destination in the list.
-def store_message(dest, msg, message_queue):
-    message_dict = message_queue.get()  # get the dictionary from the queue
-    if dest in message_dict:
-        print("dest already in list, adding msg to it")
-        message_dict[dest].append(msg)  # put the entry in the dictionary of lists of dictionaries
-        message_queue.put(message_dict)  # put the dictionary in the queue
+#this is the new receive_message
+def receive_packet (sock):
+    global client_directory
+    global routing_table
+    global message_inbox
+    print "Server is listening"
+    data, address = sock.recvfrom(10240)
+    print "Received packet, deciding what to do with it"
+    message = pickle.loads(data)
+    print "Raw message: ", str(message)
+    if type(message) is dict:
+        #packet types fall into these categories:
+        #send: client wants to send messages to another client
+        #get: client wants to view their messages
+        #server_get: propagates a get message to other servers 
+        #server_deliver: delivers a list of messages in the payload
+        #ack: signals that the payload contains the dictionary of messages to delete
+        #handshake: user enters the network, tries to register user_name, gets initial
+        #messages if registration successful
+        #routing_update: happens when new user enters, informs servers where they connected
+        #exit: user signals intent to log off
+        print "Message was a dict"
 
-    else:
-        print("dest not in list, creating new list")
-        newList = []
-        newList.append(msg)
-        message_dict[dest] = newList
-        message_queue.put(message_dict)
+        if (message['type'] == 'send'):
+            #handle send function
+            print "received send type"
+            send_handle(message, client_directory, routing_table, message_inbox) #working on
+        if (message['type'] == 'handshake'):
+            handshake_response(message, client_directory, routing_table, message_inbox)
+        '''if (message['type'] == 'get'):
+        if (message['type'] == 'server_get'):
+        if (message['type'] == 'server_deliver'):    
+        if (message['type'] == 'ack'):
+        if (message['type'] == 'routing_update'):
+        if (message['type'] == 'exit'):  '''  
 
-def handle_acknowledgement(seq_list, client, message_queue):
-    print("Acknowledging Messagees...")
-    message_dict = message_queue.get()
-    list_of_messages = message_dict[client]
-    for idx in list_of_messages[:]:
-        if idx['seq'] in seq_list:
-            list_of_messages.remove(idx)
-            print("Removed message: ", str(idx['seq']))
-    message_dict[client] = list_of_messages
-    message_queue.put(message_dict)
-
-
-    # this connection shit is fine
-try:
-    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    sock2 = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-except socket.error:
-    print ("Failed to create sockets.")
-    sys.exit()
-
-#server will listen to any IP using it's ports.
-sock.bind(('', UDP_PORT))
-sock2.bind(('', UDP_ACK_PORT))
-
-print ("server started on port: " + str(UDP_PORT))
-
-#data structure for messages per client. { client ip : [list of messages] }
-
-
-while True:
-    '''p = Process(target=receive_message, args=(message_queue,)) # start process to do listening
-    p.start() #start the listening
-    p.join() # lock access to the queue
-    p.terminate()'''
-    receive_message(message_queue)
-
-
-
-
-
-'''def listen_mode():
-    print ("listen for data....")
-    data, address = sock.recvfrom(1024)
-    #data2, addr = sock2.recvfrom(1024)
-    print ("thread start...")
-    t = threading.Thread(target = receive_message, args=[data, address])
-    #t2 = threading.Thread(target = '''
-''', args = [data2, addr])
-
-    t.start()
-    #t2.start()
-    '''
-
-'''
-def check_inbox( inbox ):
-    
-def forward_mail( dest, inbox ):
-    for message in inbox:
-        if(message['destination'] == dest):
-            sock.sendto(pickle.dumps(message).encode('utf-8'),(dest,UDP_PORT))
-            wait_for_ack()
-            del inbox[message]
-
-def wait_for_ack():
-    while True:
+def handshake_response (message, client_directory, routing_table, message_inbox):
+    #first we will check if the user name is being hosted by another server
+    if message['user_name'] not in routing_table:
+        #username was not taken, tell user it was good
+        print "username was good"
+        user_ack = {'type': 'user_good'}
+        #send message to user
+        sock.sendto(pickle.dumps(user_ack), (message['source'], CLIENT_PORT))
+        if message['user_name'] not in client_directory:
+            #this will pair up the client's username with their IP, add to directory
+            print "client added to client directory"
+            client_directory[message['user_name']] = message['source']
+        if message['user_name'] not in message_inbox:
+            #if the client had no message_inbox, make them an empty box (list)
+            print "new mailbox made for client", message['user_name']
+            message_inbox[message['user_name']] = []
+        routing_message = {'type': 'routing_update', 'server_source': socket.gethostbyname(socket.gethostname()), 'payload': routing_table, 'life_time': 0}
+        print "letting other servers know about new user via routing update"
+        #sock.sendto(pickle.dumps(routing_message), (neighbor1, SERVER_PORT))
+        #sock.sendto(pickle.dumps(routing_message), (neighbor2, SERVER_PORT))
+        print "sending out server get to find messages for client: ", message['user_name']
         
+    
 
-def listen():
-    data, addr = sock.recvfrom(1024)
-    message = pickle.loads(data.decode('utf-8'))
+#this function is responsible for dealing with a send type message
+def send_handle(message, client_directory, routing_table, message_inbox):
+    #check to see if the client's user_name is in the directory already
+    if message['destination'] in client_directory:
+        #if so, then just store their messages
+        store_message(message, message_inbox)
+    #if the client is not in the client directory:
+    else:
+        #check the routing table for the message's destination
+        server_host = get_user_host(message['destination'], routing_table)
+        #this means that a server host was found for a client
+        if server_host != '0':
+            sock.sendto(pickle.dumps(message), (server_host, SERVER_PORT))
+        #this means that we did not find a servr host for a client
+        else:
+            #so just store the message for later
+            store_message(message, message_inbox)
 
-    if(message['type'] == 'get'):
-        forward_mail(message['source'])
-    if(message['type'] == 'send'):
-        inbox.append(message)
-    if(message['type'] == "ack"):     
-while True:
-    listen()
-'''
+
+#function used to store message for a user in the inbox
+def store_message(message, message_inbox):
+    #if the user has a mailbox in the inbox
+    if message['destination'] in message_inbox:
+        print "storing a message for : ", message['destination']
+        #then we append the current message to the list of messages for the user
+        message_inbox[message['destination']].append(message)
+    else:
+        #we are going to make a mailbox for the user
+        new_list = []
+        #add the incoming message to the list for the user
+        new_list.append(message)
+        #put the user's list of messages in the dictionary under their name
+        message_inbox[message['destination']] = new_list
+
+#this function is designed to return the ip of a server hosting a user_name            
+def get_user_host(user_name, routing_table):
+    if user_name in routing_table:
+        #this will return the ip of server that is hosting the user we are asking about
+        #(relative to the server calling this function)
+        return routing_table[user_name][0]
+    else:
+        #this is a sentinel value indicating that the user could not be found
+        return '0'
+
+if __name__ == '__main__':
+
+    #try and create socket
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        #bind the socket for listening for the server
+        sock.bind(('', SERVER_PORT))
+        print "Server started on port: ", str(SERVER_PORT)
+    except socket.error:
+        #if socket creation failed, notify and break
+        print "Failed to create socket"
+        sys.exit()
+
+    while True:
+        #start listening
+        receive_packet(sock)
+
+
+                        
+                         
+
+                         
+                        
+
